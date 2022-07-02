640f96c552635b7491822b17c9f400e669280454
Mahya beh <mahyab79@gmail.com>
2022-07-03 02:01:14 +0430

    wrte the props for functionality of playing controls

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 import Player from './Components/Player';
 function App() {
-  const[songs, setSongs ] = useState([
+  const[songs ] = useState([
     {
       title:'song 1',
       artist: 'shadmehr',
@@ -29,12 +29,22 @@ function App() {
   ]);
   const [currentSongIndex , setCurrentSongIndex] = useState(0)
   const [nextSongIndex , setNextSongIndex] = useState(currentSongIndex +1)
-
+  useEffect(() =>{
+    setNextSongIndex(() => {
+      if(currentSongIndex +1 > songs.length-1) {
+        return 0;
+      }else {
+        return currentSongIndex +1
+      }
+    });
+  }, [currentSongIndex])
 
   return (
     <div className="App">
-      <Player song = {songs[currentSongIndex]} 
-      nextSong = {songs[nextSongIndex]}/>
+      <Player currentSongIndex ={currentSongIndex}
+      setCurrentSongIndex = {setCurrentSongIndex}
+      nextSongIndex = {nextSongIndex}
+      songs = {songs}/>
     </div>
   );
 }
--- src/Components/Player.js
+++ src/Components/Player.js
@@ -1,14 +1,44 @@
-import React from 'react'
+import React ,{useState , useEffect ,useRef} from 'react'
 import PlayDetails from './PlayDetails'
 import PlayControls from './PlayControls'
 function Player(props) {
+    const audioEl = useRef(null);
+    const [isPlaying,setIsPlaying]=useState(false);
+    useEffect(() => {
+        if(isPlaying) {
+            audioEl.current.play();
+        }else {
+            audioEl.current.pause();
+        }
+    });
+    const SkipSong = (forwards = true) => {
+        if(forwards ) {
+            props.setCurrentSongIndex(() =>{
+                let temp = props.currentSongIndex;
+                temp++;
+                if(temp > props.songs.length -1) {
+                    temp = 0;
+                }
+                return temp;
+            });
+        }else{
+            props.setCurrentSongIndex(() =>{
+                let temp = props.currentSongIndex;
+                temp--;
+                if(temp < 0) {
+                    temp = props.song.length -1 ;
+                }
+                return temp;
+            });
+        }
+    }
   return (
     <div className='c-player'>
-      <audio></audio>
+      <audio src={props.songs[props.currentSongIndex].src} ref={audioEl}></audio>
       <h4>playing now </h4>
-      <PlayDetails song={props.song}/>
-      <PlayControls/>
-      <p><strong> Next up : </strong>{props.nextSong.title} by {props.nextSong.artist}</p>
+      <PlayDetails song={props.songs[props.currentSongIndex]}/>
+      <PlayControls isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
+      <p><strong> Next up : </strong>{props.songs[props.nextSongIndex].title} by {props.songs[props.nextSongIndex].artist}</p>
     </div>
   )
 }
